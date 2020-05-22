1a59fe5f6e67e0a8da8ef62cd53e05f141ffd978
Angela Harper <harpaf13@gmail.com>
2020-05-22 13:18:15 +0100

    added modules to init

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,12 @@ A Python package for analyzing and visualizing xyz files. For MolSSI Workshop Py
 """
 
 # Add imports here
+# we aren't using the * because this imports literalyl everything so we use the actual file names
+# if you still want to use * you could say __all__ = ["calculate_distance", "calculate_angle"]
 from .functions import *
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
 
 # Handle versioneer
 from ._version import get_versions
